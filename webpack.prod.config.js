afc56a70c3ae01ffe4ce1b2181cf0fea7b88a65a
hhhenrysss <25966196+hhhenrysss@users.noreply.github.com>
2019-10-05 16:48:05 -0500

    production and dev mode

---
+++ webpack.prod.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "dist.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }, {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },{
+                test: /\.txt$/i,
+                use: 'raw-loader',
+            },
+        ]
+    }
+};
