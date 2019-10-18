faa021901e393a4c4e26e2c8316eaf7204a426bb
hhhenrysss <25966196+hhhenrysss@users.noreply.github.com>
2019-10-17 21:06:31 -0500

    changing to monorepo

--- components/frontPage.js
+++ components/frontPage.js
@@ -1,48 +1,61 @@
 import $ from "jquery";
 import {BasePage} from "./baseObject";
-import {CreateExplanation, CreateQuestionTitle, CreateSectionTitle} from "./miscObjects";
-import {GroupSelectionPage} from "./groupSelectionPage";
-import {GroupType} from "../storage/store";
+import {CreateExplanation, CreateOption, CreateQuestionTitle, CreateSectionTitle} from "./miscObjects";
+import {GroupType, GroupTypeUtils} from "../storage/store";
+import {StartPage} from "./startPage";
 
 export class FrontPage extends BasePage{
     constructor(elements, data) {
         super(elements);
 
-        const layoutElem = elements.textElem.append(CreateSectionTitle('Testing Website for the Confidence Elicitation Project'))
-            .append(CreateQuestionTitle('Please enter the subject ID.'))
-            .append(CreateExplanation('You must enter a subject ID.'));
-
         const inputElem = $('<input type="text">');
-        elements.textElem.append(layoutElem).append(inputElem);
+        const options = CreateOption([
+            {name: `Group 1: ${GroupType.linearNoBank}`, value: GroupType.linearNoBank},
+            {name: `Group 2: ${GroupType.linearWithBank}`, value: GroupType.linearWithBank},
+            {name: `Group 3: ${GroupType.parabolicNoBank}`, value: GroupType.parabolicNoBank},
+            {name: `Group 4: ${GroupType.parabolicWithBank}`, value: GroupType.parabolicWithBank}
+        ]);
+
+        elements.textElem
+            .append(CreateSectionTitle('Testing Website for the Confidence Elicitation Project'))
+            .append(CreateExplanation('You must enter the subject ID string and group.'))
+            .append(CreateQuestionTitle('Please enter the subject ID.'))
+            .append(inputElem)
+            .append(CreateQuestionTitle('Please select the subject group.'))
+            .append(options.jQueryObj);
+
+
         this.inputElem = inputElem;
+        this.options = options;
         this.subjectID = '';
+        this.groupSelectResult = '';
 
         this.elements = elements;
         this.data = data;
     }
 
-    idValidation(text) {
-        return text !== '';
-    }
-
     // public methods
     canProceed() {
         const inputText = this.inputElem.val();
-        if (this.idValidation(inputText)) {
+        const groupValue = this.options.value;
+        if (inputText !== '' && GroupTypeUtils.isGroupType(value)) {
             this.subjectID = inputText;
+            this.groupSelectResult = value;
             super.hideErrorMessage();
             return true;
         }
-        super.addErrorMessage('Please enter a valid Subject ID');
+        super.addErrorMessage('Please select a value before continue');
         return false;
     }
 
     record() {
         this.data.SubjectID = this.subjectID;
+        this.data.Type = this.groupSelectResult;
+        this.data.ReadableType = Object.keys(GroupType).find(key => GroupType[key] === this.groupSelectResult);
     }
 
     nextElement() {
         super.clearPage();
-        return new GroupSelectionPage(this.elements, this.data);
+        return new StartPage(this.elements, this.data);
     }
 }
