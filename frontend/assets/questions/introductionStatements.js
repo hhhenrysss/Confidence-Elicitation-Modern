ac6f8010b9f7724f73c9da9d2c816b3dcef578d3
hhhenrysss <25966196+hhhenrysss@users.noreply.github.com>
2019-11-07 02:30:23 -0600

    fixed instructions

--- frontend/assets/questions/introductionStatements.js
+++ frontend/assets/questions/introductionStatements.js
@@ -1,31 +1,23 @@
 import {groupType} from "../../storage/store";
 import {Config} from "../../configurations";
 
+const firstSentence = `You will receive $${Config.fixedPayment} for your participation in this experiment. This is yours to keep regardless of how you perform in the experiment.`;
+const secondOptionalSentence = `You will also have a virtual bank which you will receive its cash value at the end of the experiment. The bank's initial value is $${Config.initialMoney}.`;
+const thirdSentence = `<strong>The first three questions you will receive are only for practice and will not count toward your bank balance.</strong>`;
+const fourthSentence = `Your answers to non-tutorial questions will determine your final bank balance. Correct answers will increase your bank balance from the initial $${Config.initialMoney}, and incorrect answers will lower your bank balance, but the balance will never go below zero.`;
+const fifthOptionalSentence = `At the end of every ${Config.breakInterval} non-tutorial questions, you will receive a summary report of your earned reward/penalty in the most recent ${Config.breakInterval} non-tutorial questions as well as your overall bank balance up to that certain point of experiment`;
+
 export const IntroductionStatements = {
     [groupType.linearWithBank]: [
-        `You will receive $${Config.fixedPayment} for your participation in this experiment. This is yours to keep regardless of how you perform in the experiment.`,
-        `You will also have a virtual bank which you will receive its cash value at the end of the experiment. The bank's initial value is $${Config.initialMoney}.`,
-        `By answering each question, your reward or penalty from that question will increase or decrease your bank balance`,
-        `At the end of every ${Config.breakInterval} questions, you will receive a summary report of your earned reward/penalty in the most recent ${Config.breakInterval} questions as well as your overall bank balance up to that certain point of experiment`,
-        `Your answers will determine your final bank balance. Correct answers will increase your bank balance from the initial $${Config.initialMoney}, and incorrect answers will lower your bank balance, but the balance will never go below zero.`
+        firstSentence, secondOptionalSentence, thirdSentence, fourthSentence, fifthOptionalSentence
     ],
     [groupType.parabolicWithBank]: [
-        `You will receive $${Config.fixedPayment} for your participation in this experiment. This is yours to keep regardless of how you perform in the experiment.`,
-        `You will also have a virtual bank which you will receive its cash value at the end of the experiment. The bank's initial value is $${Config.initialMoney}.`,
-        `By answering each question, your reward or penalty from that question will increase or decrease your bank balance`,
-        `At the end of every ${Config.breakInterval} questions, you will receive a summary report of your earned reward/penalty in the most recent ${Config.breakInterval} questions as well as your overall bank balance up to that certain point of experiment`,
-        `Your answers will determine your final bank balance. Correct answers will increase your bank balance from the initial $${Config.initialMoney}, and incorrect answers will lower your bank balance, but the balance will never go below zero.`
+        firstSentence, secondOptionalSentence, thirdSentence, fourthSentence, fifthOptionalSentence
     ],
     [groupType.linearNoBank]: [
-        `You will receive $${Config.fixedPayment} for your participation in this experiment. This is yours to keep regardless of how you perform in the experiment.`,
-        `You will also have a virtual bank which you will receive its cash value at the end of the experiment. The bank's initial value is $${Config.initialMoney}.`,
-        `By answering each question, your reward or penalty from that question will increase or decrease your bank balance`,
-        `Your answers will determine your final bank balance. Correct answers will increase your bank balance from the initial $${Config.initialMoney}, and incorrect answers will lower your bank balance, but the balance will never go below zero.`
+        firstSentence, thirdSentence, fourthSentence
     ],
     [groupType.parabolicNoBank]: [
-        `You will receive $${Config.fixedPayment} for your participation in this experiment. This is yours to keep regardless of how you perform in the experiment.`,
-        `You will also have a virtual bank which you will receive its cash value at the end of the experiment. The bank's initial value is $${Config.initialMoney}.`,
-        `By answering each question, your reward or penalty from that question will increase or decrease your bank balance`,
-        `Your answers will determine your final bank balance. Correct answers will increase your bank balance from the initial $${Config.initialMoney}, and incorrect answers will lower your bank balance, but the balance will never go below zero.`
+        firstSentence, thirdSentence, fourthSentence
     ]
 };
